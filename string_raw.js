b073a0804072fc95dfeab74943339073f644daf5
syd9538 <syd9538@gmail.com>
2022-01-27 23:12:11 +0900

    feat : template literal(string raw)

---
+++ string_raw.js
@@ -0,0 +1,11 @@
+// const res = `순서가 ${1}이렇게 ${2}\n\n`;
+// console.log(res)
+
+// const res = String.raw`순서가 ${1}이렇게 ${2}\n\n`;
+// console.log(res)
+
+
+console.log(`Hello\nWorld!`)
+console.log(String.raw `Hello\nWorld!`)
+console.log(String.raw `Hello
+World!`)
