a9b54ee55606f7cedfbf911bb6b272491ec73bd8
syd9538 <syd9538@gmail.com>
2022-02-24 23:24:43 +0900

    feat : 함수선언문과 스코프

---
+++ 함수선언문과스코프.js
@@ -0,0 +1,14 @@
+if(true){
+    a()
+    function a(){console.log(true)}
+}
+
+a()
+
+//'strict mode가 아닌경우 'sloopy mode에서는 브라우저 마다 다른 동작. 예상이 안됨...
+//strict mode : 함수 선언문도 블락 스코프에 갇힌다.
+
+//es6 에서는 함수 선언문 쓰지 말고,
+//arrow function
+//객체 : 메소드 축약형
+// 생성자함수 class
